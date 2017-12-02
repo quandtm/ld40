7a9bae2e2a5e58a8deda8d54afc47b4e60bf18fe
Michael Quandt <michael@mquandt.com>
2017-12-03 10:33:54 +1100

    Main & result menus

---
+++ ld40/Assets/src/MainMenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void BeginGame(string difficulty)
+    {
+        SceneManager.LoadScene("GameLogic", LoadSceneMode.Single);
+        SceneManager.LoadScene("CamTest", LoadSceneMode.Additive);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
