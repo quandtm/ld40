126db962b37fef8b2251176a50dd499c80f39a49
Michael Quandt <michael@mquandt.com>
2017-12-02 19:15:41 +1100

    Look rotation + input thresholds

--- ld40/Assets/src/PlayerBrain.cs
+++ ld40/Assets/src/PlayerBrain.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+enum LookDirection
+{
+    Left = 0,
+    Right = 180,
+    Background = 90,
+    Foreground = 270
+}
+
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerBrain : MonoBehaviour
 {
     public float Speed = 1f;
+	public float HorizontalDeadzone = 0.3f;
+	public float VerticalThreshold = 0.8f;
 	public float HeightOffset = 1f;
+	public float LeftYRotation = 0f;
 
 	private Rewired.Player player;
 	private Rigidbody body;
 	private float baseHeight = 0f;
+	private LookDirection lookDir = LookDirection.Left;
 
 	private StairZone stairZone;
 	private bool vMovHandled = false;
@@ -27,15 +39,16 @@ public class PlayerBrain : MonoBehaviour
 
         float hmov = player.GetAxis(0);
         float moveDist = 0f;
-        if (Mathf.Abs(hmov) > float.Epsilon)
+        if (Mathf.Abs(hmov) > HorizontalDeadzone)
         {
             moveDist = hmov * Time.deltaTime * Speed;
+			lookDir = moveDist > 0f ? LookDirection.Right : LookDirection.Left;
             moved = true;
         }
 
         float vmov = player.GetAxis(1);
         bool vmovJustPressed = false;
-        if (Mathf.Abs(vmov) > 0.5f)
+        if (Mathf.Abs(vmov) > VerticalThreshold)
         {
             if (!vMovHandled)
             {
@@ -60,6 +73,8 @@ public class PlayerBrain : MonoBehaviour
             }
             else
             {
+                lookDir = vmov > 0f ? LookDirection.Background : LookDirection.Foreground;
+				moved = true;
             }
         }
 
@@ -68,6 +83,8 @@ public class PlayerBrain : MonoBehaviour
             var pos = body.position;
             pos.x += moveDist;
             pos.y = baseHeight + HeightOffset;
+            var rot = Quaternion.Euler(0f, LeftYRotation + (float)lookDir, 0f);
+			body.MoveRotation(rot);
             body.MovePosition(pos);
         }
     }
