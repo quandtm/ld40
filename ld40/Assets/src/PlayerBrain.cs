f76938df188094ccf5d72ccb204bc5712936ff18
Michael Quandt <michael@mquandt.com>
2017-12-02 18:56:33 +1100

    Rewired + floor/stair movement

---
+++ ld40/Assets/src/PlayerBrain.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBrain : MonoBehaviour
+{
+    public float Speed = 1f;
+	public float HeightOffset = 1f;
+
+	private Rewired.Player player;
+	private Rigidbody body;
+	private float baseHeight = 0f;
+
+	private StairZone stairZone;
+	private bool vMovHandled = false;
+
+	void Start()
+	{
+		player = Rewired.ReInput.players.GetPlayer("MainPlayer");
+		body = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        bool moved = false;
+
+        float hmov = player.GetAxis(0);
+        float moveDist = 0f;
+        if (Mathf.Abs(hmov) > float.Epsilon)
+        {
+            moveDist = hmov * Time.deltaTime * Speed;
+            moved = true;
+        }
+
+        float vmov = player.GetAxis(1);
+        bool vmovJustPressed = false;
+        if (Mathf.Abs(vmov) > 0.5f)
+        {
+            if (!vMovHandled)
+            {
+                vMovHandled = true;
+                vmovJustPressed = true;
+            }
+        }
+        else
+        {
+            vMovHandled = false;
+        }
+
+        if (vmovJustPressed)
+        {
+            if (stairZone != null)
+            {
+                if (vmov > 0f)
+                    baseHeight = stairZone.GetUpZoneHeight();
+                else
+                    baseHeight = stairZone.GetDownZoneHeight();
+                moved = true;
+            }
+            else
+            {
+            }
+        }
+
+        if (moved)
+        {
+            var pos = body.position;
+            pos.x += moveDist;
+            pos.y = baseHeight + HeightOffset;
+            body.MovePosition(pos);
+        }
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		var zone = other.GetComponent<StairZone>();
+		if (zone != null)
+			stairZone = zone;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		var zone = other.GetComponent<StairZone>();
+		if (zone != null)
+			stairZone = null;
+	}
+}
---
+++ ld40/Assets/src/StairZone.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairZone : MonoBehaviour
+{
+	public float Height;
+	public GameObject Upstairs;
+
+	private StairZone upZone;
+	private StairZone downZone;
+
+    public bool HasUpZone { get { return upZone != null; } }
+    public bool HasDownZone { get { return downZone != null; } }
+
+	public float GetUpZoneHeight()
+	{
+		return upZone!=null ? upZone.Height : Height;
+	}
+
+	public float GetDownZoneHeight()
+	{
+		return downZone != null ? downZone.Height : Height;
+	}
+
+	void Start()
+	{
+		Height = transform.position.y;
+        if (Upstairs != null)
+		{
+            upZone = Upstairs.GetComponent<StairZone>();
+            if (upZone != null)
+                upZone.downZone = this;
+            else
+                Debug.LogErrorFormat("Upstairs object on {0} must have a StairZone component", name);
+		}
+	}
+}
