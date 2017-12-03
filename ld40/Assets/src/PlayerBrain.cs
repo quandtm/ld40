3418e03ce6be772074c768e672136803f8e16992
Michael Quandt <michael@mquandt.com>
2017-12-03 15:15:50 +1100

    Integrate animations + block for scaring

--- ld40/Assets/src/PlayerBrain.cs
+++ ld40/Assets/src/PlayerBrain.cs
@@ -27,63 +27,60 @@ public class PlayerBrain : MonoBehaviour
     private bool vMovHandled = false;
     private Hauntable[] proxHaunts = new Hauntable[2]; // 0=fore, 1=back
 
-    public GameObject mesh;
-    private Animator anim;
-    private int speedParamHash;
-    private int scareHash;
+    public PlayerMeshAnim childMesh;
 
     void Start()
     {
         body = GetComponent<Rigidbody>();
-        anim = mesh.GetComponent<Animator>();
-        speedParamHash = Animator.StringToHash("MoveSpeed");
-        scareHash = Animator.StringToHash("DoScare");
     }
 
     void Update()
     {
         if (GameDirector.Instance.CurrentPhase == Phase.Play)
         {
-            if (Input.GetButtonUp("Fire1"))
+            if (!childMesh.IsInAnim)
             {
-                bool shouldAnim = false;
-                if (lookDir == LookDirection.Background)
+                if (Input.GetButtonUp("Fire1"))
                 {
-                    if (proxHaunts[1] != null && !proxHaunts[1].PreviouslyInteracted)
+                    bool shouldAnim = false;
+                    if (lookDir == LookDirection.Background)
                     {
-                        // TODO: Animation based on IsPossessed state BEFORE change
-                        if (proxHaunts[1].IsPossessed)
+                        if (proxHaunts[1] != null && !proxHaunts[1].PreviouslyInteracted)
                         {
-                            proxHaunts[1].IsPossessed = false;
-                            proxHaunts[1].PreviouslyInteracted = true;
-                            Debug.Log("Ghost eliminated");
-                            shouldAnim = true;
+                            // TODO: Animation based on IsPossessed state BEFORE change
+                            if (proxHaunts[1].IsPossessed)
+                            {
+                                proxHaunts[1].IsPossessed = false;
+                                proxHaunts[1].PreviouslyInteracted = true;
+                                Debug.Log("Ghost eliminated");
+                                shouldAnim = true;
+                            }
                         }
                     }
-                }
-                else if (lookDir == LookDirection.Foreground)
-                {
-                    if (proxHaunts[0] != null && !proxHaunts[0].PreviouslyInteracted)
+                    else if (lookDir == LookDirection.Foreground)
                     {
-                        // TODO: Animation based on IsPossessed state BEFORE change
-                        if (proxHaunts[0].IsPossessed)
+                        if (proxHaunts[0] != null && !proxHaunts[0].PreviouslyInteracted)
                         {
-                            proxHaunts[0].IsPossessed = false;
-                            proxHaunts[0].PreviouslyInteracted = true;
-                            Debug.Log("Ghost eliminated");
-                            shouldAnim = true;
+                            // TODO: Animation based on IsPossessed state BEFORE change
+                            if (proxHaunts[0].IsPossessed)
+                            {
+                                proxHaunts[0].IsPossessed = false;
+                                proxHaunts[0].PreviouslyInteracted = true;
+                                Debug.Log("Ghost eliminated");
+                                shouldAnim = true;
+                            }
                         }
                     }
-                }
 
-                if (shouldAnim)
-                    anim.SetTrigger(scareHash);
-            }
-            else
-            {
-                float speed = body.velocity.magnitude;
-                if (speed > 0f)
-                    anim.SetFloat(speedParamHash, speed > HorizontalDeadzone ? speed : 0f);
+                    if (shouldAnim)
+                        childMesh.PlayScare();
+                }
+                else
+                {
+                    float speed = body.velocity.magnitude;
+                    if (speed > 0f)
+                        childMesh.SetSpeed(speed > HorizontalDeadzone ? speed : 0f);
+                }
             }
         }
     }
@@ -92,61 +89,64 @@ public class PlayerBrain : MonoBehaviour
     {
         if (GameDirector.Instance.CurrentPhase == Phase.Play)
         {
-            float vmov = Input.GetAxis("Vertical");
-            bool vmovJustPressed = false;
-            if (Mathf.Abs(vmov) > VerticalThreshold)
+            if (!childMesh.IsInAnim)
             {
-                if (!vMovHandled)
+                float vmov = Input.GetAxis("Vertical");
+                bool vmovJustPressed = false;
+                if (Mathf.Abs(vmov) > VerticalThreshold)
                 {
-                    vMovHandled = true;
-                    vmovJustPressed = true;
-                }
-            }
-            else
-            {
-                vMovHandled = false;
-
-                float hmov = Input.GetAxis("Horizontal");
-                float absHmov = Mathf.Abs(hmov);
-                if (absHmov > HorizontalDeadzone)
-                {
-                    float moveDist = Mathf.Sign(hmov) * ((absHmov - .3f) / .7f) * Speed;
-                    lookDir = moveDist > 0f ? LookDirection.Right : LookDirection.Left;
-                    body.velocity = new Vector3(moveDist, 0f, 0f);
-                    var rot = Quaternion.Euler(0f, LeftYRotation + (float)lookDir, 0f);
-                    body.rotation = rot;
-                }
-            }
-
-            if (vmovJustPressed)
-            {
-                if (stairZone != null)
-                {
-                    if (vmov > 0f)
-                        baseHeight = stairZone.GetUpZoneHeight();
-                    else
-                        baseHeight = stairZone.GetDownZoneHeight();
-
-                    body.velocity = Vector3.zero;
-                    var p = transform.position;
-                    p.y = baseHeight + HeightOffset;
-                    transform.position = p;
+                    if (!vMovHandled)
+                    {
+                        vMovHandled = true;
+                        vmovJustPressed = true;
+                    }
                 }
                 else
                 {
-                    if (vmov > 0f)
+                    vMovHandled = false;
+
+                    float hmov = Input.GetAxis("Horizontal");
+                    float absHmov = Mathf.Abs(hmov);
+                    if (absHmov > HorizontalDeadzone)
                     {
-                        if (proxHaunts[1] != null)
-                            lookDir = LookDirection.Background;
+                        float moveDist = Mathf.Sign(hmov) * ((absHmov - .3f) / .7f) * Speed;
+                        lookDir = moveDist > 0f ? LookDirection.Right : LookDirection.Left;
+                        body.velocity = new Vector3(moveDist, 0f, 0f);
+                        var rot = Quaternion.Euler(0f, LeftYRotation + (float)lookDir, 0f);
+                        body.rotation = rot;
+                    }
+                }
+
+                if (vmovJustPressed)
+                {
+                    if (stairZone != null)
+                    {
+                        if (vmov > 0f)
+                            baseHeight = stairZone.GetUpZoneHeight();
+                        else
+                            baseHeight = stairZone.GetDownZoneHeight();
+
+                        body.velocity = Vector3.zero;
+                        var p = transform.position;
+                        p.y = baseHeight + HeightOffset;
+                        transform.position = p;
                     }
                     else
                     {
-                        if (proxHaunts[0] != null)
-                            lookDir = LookDirection.Foreground;
-                    }
+                        if (vmov > 0f)
+                        {
+                            if (proxHaunts[1] != null)
+                                lookDir = LookDirection.Background;
+                        }
+                        else
+                        {
+                            if (proxHaunts[0] != null)
+                                lookDir = LookDirection.Foreground;
+                        }
 
-                    var rot = Quaternion.Euler(0f, LeftYRotation + (float)lookDir, 0f);
-                    body.rotation = rot;
+                        var rot = Quaternion.Euler(0f, LeftYRotation + (float)lookDir, 0f);
+                        body.rotation = rot;
+                    }
                 }
             }
         }
---
+++ ld40/Assets/src/PlayerMeshAnim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerMeshAnim : MonoBehaviour
+{
+	private int scareAnimHash;
+	private int moveBlendHash;
+	private Animator anim;
+
+	public bool IsInAnim = false;
+
+	void Start()
+	{
+		moveBlendHash = Animator.StringToHash("MoveSpeed");
+		scareAnimHash = Animator.StringToHash("DoScare");
+		anim = GetComponent<Animator>();
+	}
+    public void SetSpeed(float speed)
+    {
+		if (!IsInAnim)
+			anim.SetFloat(moveBlendHash, speed);
+    }
+
+    public void PlayScare()
+    {
+		SetSpeed(0f);
+		anim.SetTrigger(scareAnimHash);
+        IsInAnim = true;
+    }
+
+	public void ScareFinished()
+	{
+		IsInAnim = false;
+	}
+}
